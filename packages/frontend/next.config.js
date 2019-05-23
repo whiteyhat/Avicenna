9b53a032dd5e7e0b6a24e8fd273e03bc44c5ebd9
Carlos Roldan <carlosroldan26396@gmail.com>
2019-05-23 21:00:23 +0100

    Update next.config.js

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,17 +1,24 @@
 // next.config.js
-const withCSS = require('@zeit/next-css')
-module.exports = withCSS({
-    webpack: function (config) {
-        config.module.rules.push({
-            test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-            use: {
-                loader: 'url-loader',
-                options: {
-                    limit: 100000,
-                    name: '[name].[ext]'
-                }
-            }
-        })
-        return config
+const withCSS = require('@zeit/next-css');
+
+function HACK_removeMinimizeOptionFromCssLoaders(config) {
+  console.warn(
+    'HACK: Removing `minimize` option from `css-loader` entries in Webpack config',
+  );
+  config.module.rules.forEach(rule => {
+    if (Array.isArray(rule.use)) {
+      rule.use.forEach(u => {
+        if (u.loader === 'css-loader' && u.options) {
+          delete u.options.minimize;
+        }
+      });
     }
-})
+  });
+}
+
+module.exports = withCSS({
+  webpack(config) {
+    HACK_removeMinimizeOptionFromCssLoaders(config);
+    return config;
+  },
+});
