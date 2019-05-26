51021bbbd97ec81f460e7082419d93e5688d1794
Carlos Roldan <carlosroldan26396@gmail.com>
2019-05-26 15:50:49 +0100

    Update routes.js

--- start/routes.js
+++ start/routes.js
@@ -22,16 +22,15 @@ Route.on('/profile').render('profile').middleware(['auth'])
 Route.get('/staff', 'UserController.staff').middleware(['auth'])
 Route.on('/new-passport').render('index').middleware(['auth'])
 
-Route.post('/delete', 'UserController.deleteAccount')
-Route.post('/create-new', 'UserController.createUser')
-Route.post('/login', 'UserController.login')
-Route.post('/edit', 'UserController.edit')
-Route.post('/signup', 'UserController.signup')
-Route.post('/new', 'UserController.newPassport')
 
 Route.group(() => {
   Route.post('auth/verify-signature', 'UserController.digitalSignature')
   Route.post('demo/doctor', 'UserController.demoDoctor')
   Route.post('demo/admin', 'UserController.demoAdmin')
   Route.post('auth/logout', 'UserController.logout')
+  Route.post('auth/login', 'UserController.login')
+  Route.post('passport/new', 'UserController.newPassport')
+  Route.post('profile/edit', 'UserController.edit')
+  Route.post('passport/pay', 'UserController.paySatellite')
+
 }).prefix('api/v0')
