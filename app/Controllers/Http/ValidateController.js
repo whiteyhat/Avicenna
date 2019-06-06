fc362cc6998567b393e8073711b1b061d88b93ab
Carlos Roldan <carlosroldan26396@gmail.com>
2019-06-06 16:26:10 +0200

    Update ValidateController.js

--- app/Controllers/Http/ValidateController.js
+++ app/Controllers/Http/ValidateController.js
@@ -1,60 +1,113 @@
-'use strict'
+/**
+Copyright 2019 Carlos Roldan Torregrosa
 
-const axios = require('axios');
-const sha256 = require('hash.js/lib/hash/sha/256');
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+"use strict";
+
+const axios = require("axios");
+const sha256 = require("hash.js/lib/hash/sha/256");
+const Logger = use("Logger");
+
+/**
+Class to perform validation optiion for the Blockstream Satellite certification
+ */
 class ValidateController {
 
-    async validate({ request, response }) {
-        const { uuid, authtoken, filehash } = request.all()
+    /**
+    Controller to validate the Blocstream Satellite data outputs
+     */
+  async validate({ request, response }) {
 
-        let res = {
-            message: '',
-            satellite: {
-                txFound: false,
-                isPaid: false,
-            },
-            ipfs: {
-                isSha256Ok: false
-            }
+      try {
+
+    // Get the body request
+    const { uuid, authtoken, filehash } = request.all();
+
+    // Instantiate teh response body
+    let res = {
+      message: "",
+      satellite: {
+        txFound: false,
+        isPaid: false
+      },
+      ipfs: {
+        isSha256Ok: false
+      }
+    };
+
+    // HTTP Request to resolve the data body from the satellite
+    const result = await axios.get(
+      `https://api.blockstream.space/order/${uuid}`,
+      {
+        //   use the auth token within for the auth headers
+        headers: {
+          "X-Auth-Token": authtoken
+        },
+        validateStatus: function(status) {
+          return true;
         }
+      }
+    );
 
-        const result = await axios.get(`https://api.blockstream.space/order/${uuid}`, {
-            headers: {
-                'X-Auth-Token': authtoken
-            },
-            validateStatus: function (status) {
-                return true;
-            },
-        })
+    // Get the data status
+    const data = result.data;
 
-        const data = result.data
-        res.message = data.message
+    // AAdd the data status within the response body
+    res.message = data.message;
 
-        if (result.status !== 200) {
-            res.message = data.errors[0].detail
-            return response.status(200).send(res)
-        }
+    // If there is any error in the HTTP request
+    if (result.status !== 200) {
+    
+        // Add the error log in the response body
+      res.message = data.errors[0].detail;
 
-        res.satellite.txFound = true
-
-        if (data.status !== 'sent') {
-            res.message = "Satellite tx not paid"
-            return response.status(200).send(res)
-        }
-
-        res.satellite.isPaid = true
-
-        const filehash256 = sha256().update(filehash).digest('hex')
-        if (data.message_digest === filehash256) {
-            res.ipfs.isSha256Ok = true
-            res.message = 'Successfully validated'
-        }
-
-        return response.status(200).send(res)
+        // Return the response body
+      return response.status(200).send(res);
     }
 
+    // If the Blockstream Satellite has a tx 
+    // update the response body 
+    res.satellite.txFound = true;
+
+    // If there was any error within the satellite payment (lightning)
+    if (data.status !== "sent") {
+
+        // Update the response body
+      res.message = "Satellite tx not paid";
+
+        // Return the response body
+      return response.status(200).send(res);
+    }
+
+    // If the Blockstream Satellite was paid (using lightning)
+    // update the response body
+    res.satellite.isPaid = true;
+
+    // Make a hash (SHA-256) of the IPFS HASH obtained in the request body
+    const filehash256 = sha256().update(filehash).digest("hex");
+
+    // If there is an exact match between the SHA-256 hash from the 
+    // blockstream satellite and the IPFS hashed hash, then the 
+    // validation has been successful 
+    if (data.message_digest === filehash256) {
+
+        // Update the response body with successful details
+      res.ipfs.isSha256Ok = true;
+      res.message = "Successfully validated";
+    }
+
+    // Return the response body to the user
+    return response.status(200).send(res);
+
+    catch(error){
+        Logger.error(error)
+    }
+  }
 }
 
-module.exports = ValidateController
+module.exports = ValidateController;
