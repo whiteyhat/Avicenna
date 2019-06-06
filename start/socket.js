e73b02181aa97d2366069456b53e1816ef4148a5
Carlos Roldan <carlosroldan26396@gmail.com>
2019-06-06 16:26:20 +0200

    Update socket.js

--- start/socket.js
+++ start/socket.js
@@ -1,3 +1,12 @@
+/**
+Copyright 2019 Carlos Roldan Torregrosa
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 "use strict";
 
 const Invoice = use("App/Models/Invoice");
@@ -40,19 +49,27 @@ const initWS = async () => {
     websocket.on("data", async data => {
       // If invoice is paid
       if (data.is_confirmed) {
-        // If invoice is equal to 1000 satoshis = If invoice for standard account creation
+        // If a lightning invoice has been paid for a certification using Blockstream Satellite
         if (data.description ==="Upload Avicenna's Passport to Blockstream Satellite") {
           Logger.info("Invoice paid for passport generation using Blockstream Satellite");
 
           try {
             // Find invoice by ID and save it into db as paid
             const invoice = await Invoice.findBy("invoiceId", data.id);
+
+            // Get the socket ID from the invoices
             const socketId = `invoice#${invoice.socketId}`;
+
+            // Update the invoice to paid
             invoice.is_paid = true;
+
+            // Save the invoice
             await invoice.save();
 
+            // Get the user by his/her session id
             const user = await User.findBy("id", invoice.user_id);
 
+            // Get the lnd instance
             const lnd = LightningService.getLndInstance();
 
             // Upload the hash to the Blockstream Satellite
@@ -70,13 +87,16 @@ const initWS = async () => {
                 // The pr must be sent to the client to pay the satellite operation
                 const pr = body.data.lightning_invoice["payreq"];
 
+                // Pay the blockstream satellite payment request
                 await payInvoice({ lnd, request: pr });
 
+                // Emit data to the ws channel
                 Ws.getChannel("invoice")
                   .topic("invoice")
                   .emitTo(
                     "invoicePaid",
                     {
+                      // send the UUID & auth token
                       uuid,
                       authToken
                     },
@@ -89,37 +109,67 @@ const initWS = async () => {
           }
         }
 
+        // If a lightning invoice has been paid for a certification using Open Time Stamps
         if (data.description.includes("Open Time Stamps")) {
           Logger.info("Invoice paid for passport generation usin Open Time Stamps");
 
+          // Split the data form the description until getting the file id
           const filename = data.description.split("| ");
+
+          // Get the path for the data to verify using OTS
           const path = 'public/temp/' + filename[1] + '.pdf'
 
           try {
             // Find invoice by ID and save it into db as paid
             const invoice = await Invoice.findBy("invoiceId", data.id);
+
+
+            // Get the socket ID from the invoices
             const socketId = `invoice#${invoice.socketId}`;
+
+            // Update the invoice to paid
             invoice.is_paid = true;
+
+            // Save the invoice
             await invoice.save();
 
+            // Get the user by his/her session id
             const user = await User.findBy("id", invoice.user_id);
 
+            // Read the file of the data to verify using OTS
             const file = fs.readFileSync(path)
+
+            // Create an OTS stamp
             const detached = OpenTimestamps.DetachedTimestampFile.fromBytes(new OpenTimestamps.Ops.OpSHA256(), file);
             OpenTimestamps.stamp(detached).then( ()=>{
+
+              // Serialize the OTS stamp proof
               const fileOts = detached.serializeToBytes();
+
+              // Generate a temporary path to store the OTS stamp proof
               const frontPath = '/temp/'+ Date.now().toString()+'.ots'
+
+              // Generate a relative path to access the stamp proof from the front
               const verificationPath = 'public'+ frontPath
+
+              // Write the OTS stamp proof in the previous path to serve it as a gateway
               fs.writeFile( verificationPath, fileOts, async function (err) {
                   if (err) throw err;
                   // Upload the hash of the verification hash to IPFS to have a gateway
                   await LightningService.uploadToIPFS(verificationPath)
                     .then(function(result) {
-                      // automate the self-destruction operation
+
+                      // automate the self-destruction operation from both paths
                       PdfService.autoDeletePdf(verificationPath);
                       PdfService.autoDeletePdf(frontPath);
+
+                      // Log the IPFS hash gateway
                       Logger.info("IPFS HASH: " + result.hash);
+
+                      // Emit the data outputs through the ws channel
                        Ws.getChannel("invoice").topic("invoice").emitTo("invoicePaid",{
+
+                        //  send the temp gateway and the permanent gateway of the OTS stamp proof
                           verification: frontPath,
                           verificationIpfsPath: result.hash
                         },
