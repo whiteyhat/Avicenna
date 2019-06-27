476abe97f8714fae373e60a0b17f574a73197ee2
Carlos Roldan <carlosroldan26396@gmail.com>
2019-06-27 21:28:26 +0200

    Update cors.js

--- config/cors.js
+++ config/cors.js
@@ -16,7 +16,7 @@ module.exports = {
   | Function - Receives the current origin and should return one of the above values.
   |
   */
-  origin: ['https://browser.blockstack.org'],
+  origin: true,
 
   /*
   |--------------------------------------------------------------------------
@@ -47,7 +47,7 @@ module.exports = {
   | Function - Receives the current header and should return one of the above values.
   |
   */
-  headers: false,
+  headers: true,
 
   /*
   |--------------------------------------------------------------------------
